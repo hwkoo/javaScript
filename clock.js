a89927116794ad335d429d1fe17ca6fc187b51f6
unknown <hwkoo2312@gmail.com>
2018-12-08 18:44:33 +0900

    Enter

--- clock.js
+++ clock.js
@@ -7,8 +7,8 @@ function getTime(){
     const hours = date.getHours();
     const seconds = date.getSeconds();
     clockTitle.innerText = `${
-        hours < 10 ? `0${hours
-        }` : `${hours}`}:${minutes}:${
+        hours < 10 ? `0${hours}` : `${hours}`
+    }:${minutes}:${
         seconds < 10 ? `0${seconds}` : `${seconds}`
     }`;
 
